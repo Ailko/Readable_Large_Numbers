816480690159f6881e2bac7430180bcf7525945d
Ailko <ailko.claeys@outlook.com>
2020-05-10 21:14:44 +0200

    Further compacting

--- ReadableLargeNumbers/ReadableLargeNumbers/Program.cs
+++ ReadableLargeNumbers/ReadableLargeNumbers/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Numerics;
+using System.Resources;
 
 namespace Readable_numbers
 {
     class Program
     {
+        //Strings to append for names
         static public string[][] computelarger = new string[][] {
             new string[]{ "un", "duo", "tre", "quattuor", "quinqua", "se", "septe", "octo", "nove" },
             new string[]{ "deci", "viginti", "triginta", "quadraginta", "quinquaginta", "sexaginta", "septuaginta", "octoginta", "nonaginta" },
@@ -25,7 +27,8 @@ namespace Readable_numbers
                 Console.WriteLine("Please enter your number. (-1 to exit)");
                 value = BigInteger.Parse(Console.ReadLine());
                 Console.WriteLine();
-                int sectorcount = 0;
+
+                int sectorcount = 0;    //Put generation of the number in {} to save memory space
                 {
                     BigInteger valuecopy = value;
                     ulong length = 0;
@@ -59,12 +62,12 @@ namespace Readable_numbers
                             concatstring = Singles(sectorcount - 2);
                         }
                         Console.WriteLine($"{value / BigInteger.Pow(10, (sectorcount - 1) * 3)} {concatstring}illion");
-                        value -= (value / BigInteger.Pow(10, (sectorcount - 1) * 3)) * BigInteger.Pow(10, (sectorcount - 1) * 3);
+                        value -= (value / BigInteger.Pow(10, (sectorcount - 1) * 3)) * BigInteger.Pow(10, (sectorcount - 1) * 3);   //Substract used value from number
                     }
                     sectorcount--;
                 }
 
-                if (value / 1000 != 0)
+                if (value / 1000 != 0)      //Language being irregular, as usual *rolls eyes*
                 {
                     Console.WriteLine($"{value / 1000} thousand");
                     value -= (value / 1000) * 1000;
@@ -85,23 +88,28 @@ namespace Readable_numbers
         static string Over1000(int sectorcount)
         {
             string result = "";
-            int sectorTh = sectorcount / 1000;
-            int rest = sectorcount - sectorTh * 1000;
+            int sectorTh = sectorcount / 1000;      //Split section that needs illinillion after it instead of illion
+            int rest = sectorcount - sectorTh * 1000;       //"Regular" big numbers (max 10^2997)
 
             if (rest > 99)
             {
-                result += Over100(rest) + "i";
+                result += Over100(rest);
             }
             else if (rest > 9)
             {
-                result += Over10(rest) + computelarger[1][(rest - (rest - (rest / 10) * 10)) / 10 - 1].Substring(computelarger[1][(rest - (rest - (rest / 10) * 10)) / 10 - 1].Length - 1);
+                result += Over10(rest);
             }
             else if (rest > 0)
             {
-                result += Singles(rest) + "illi";
+                result += Singles(rest);
             }
 
-            if (sectorTh > 999)
+            if(rest != 0)
+            {
+                result += "illion";
+            }
+
+            if (sectorTh > 999)     //Honestly don't know why I added this, most programs refuse to generate numbers this big
             {
                 result += Over1000(sectorTh);
             }
@@ -126,34 +134,18 @@ namespace Readable_numbers
         static string Over100(int sectorcount)
         {
             string result = "";
-            int sectorH = sectorcount / 100;
-            int sectorTU = sectorcount - sectorH * 100;
+            int sectorH = sectorcount / 100;        //Split hunderds of the sectors
+            int sectorT = sectorcount / 10 - sectorH * 10;     //Splits Tens
+            int sectorU = sectorcount - sectorT * 10 - sectorH * 100;  //Split Units
 
-            if (sectorTU > 9)
+            if (sectorT > 0)
             {
-                result += Over10(sectorTU);
-                result += computelarger[1][(sectorTU - (sectorTU - (sectorTU / 10) * 10)) - 1][computelarger[1][(sectorTU - (sectorTU - (sectorTU / 10) * 10)) - 1].Length - 1];
+                result += Over10(sectorT * 10 + sectorU);
+                result += computelarger[1][sectorT - 1][computelarger[1][sectorT - 1].Length - 1];    //Add last letter (is excluded in Over10 because it is deleted for numbers under 10)
             }
-            else if (sectorTU > 0)
+            else if (sectorU > 0)
             {
-                result += computelarger[0][sectorTU - 1];
-                switch ((int)(sectorTU))
-                {
-                    case 3:
-                    case 6:
-                        if (mods[1][sectorTU - 1][0] != '\0')
-                        {
-                            result += mods[1][sectorTU - 1][0];
-                        }
-                        break;
-                    case 7:
-                    case 9:
-                        if (mods[1][sectorTU - 1][1] != '\0')
-                        {
-                            result += mods[1][sectorTU - 1][1];
-                        }
-                        break;
-                }
+                result += Irregularities(sectorU, 1);
             }
 
             result += computelarger[2][sectorH - 1];
@@ -164,32 +156,15 @@ namespace Readable_numbers
         static string Over10(int sectorcount)
         {
             string result = "";
-            int sectorT = sectorcount / 10;
-            int sectorU = sectorcount - sectorT * 10;
+            int sectorT = sectorcount / 10;     //Split tens off
+            int sectorU = sectorcount - sectorT * 10;       //Units
 
             if (sectorU != 0)
             {
-                result += computelarger[0][sectorU - 1];
-                switch ((int)(sectorU))
-                {
-                    case 3:
-                    case 6:
-                        if (mods[0][sectorT - 1][0] != '\0')
-                        {
-                            result += mods[0][sectorT - 1][0];
-                        }
-                        break;
-                    case 7:
-                    case 9:
-                        if (mods[0][sectorT - 1][1] != '\0')
-                        {
-                            result += mods[0][sectorT - 1][1];
-                        }
-                        break;
-                }
+                result += Irregularities(sectorU, 0);
             }
 
-            if (sectorcount - (sectorcount - (sectorcount / 10) * 10) != 0)
+            if (sectorT != 0)
             {
                 result += computelarger[1][sectorT - 1].Substring(0, computelarger[1][sectorT - 1].Length - 1);
             }
@@ -200,6 +175,29 @@ namespace Readable_numbers
         static string Singles(int sectorcount)
         {
             string result = smallerthan10[sectorcount - 1];
+            return result;
+        }
+
+        static string Irregularities(int units, int noT)
+        {
+            string result = computelarger[0][units - 1];
+            switch (units)        //Language irregularities intensify
+            {
+                case 3:
+                case 6:
+                    if (mods[noT][units - 1][0] != '\0')
+                    {
+                        result += mods[noT][units - 1][0];
+                    }
+                    break;
+                case 7:
+                case 9:
+                    if (mods[noT][units - 1][1] != '\0')
+                    {
+                        result += mods[noT][units - 1][1];
+                    }
+                    break;
+            }
             return result;
         }
     }
