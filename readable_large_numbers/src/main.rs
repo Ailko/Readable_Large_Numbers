b846ce6762b0a59244883f33a6a1df5875957c25
Ailko <ailko.claeys@outlook.com>
2022-09-03 11:22:42 +0200

    Create necessary constants

--- readable_large_numbers/src/main.rs
+++ readable_large_numbers/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    const prefix_over_9 = (
+        ("un", "duo", "tre", "quattuor", "quinqua", "se", "septe", "octo", "nove"),
+        ("deci", "viginti", "triginta", "quadraginta", "quinquaginta", "sexaginta", "septuaginta", "octoginta", "nonaginta"),
+        ("cent", "ducent", "trecent", "quadringent", "quingent", "sescent", "septingenti", "octingent", "nongent"),
+    )
+    const prefix_under_10 = ("m", "b", "tr", "quadr", "quint", "sext", "sept", "oct", "non")
+    const modifiers = (
+        (
+            ("", "n"),
+            ("s", "m"),
+            ("s", "n"),
+            ("s", "n"),
+            ("s", "n"),
+            ("", "n"),
+            ("", "n"),
+            ("x", "m"),
+            ("", ""),
+        ),
+        (
+            ("x", "n"),
+            ("", "n"),
+            ("s", "n"),
+            ("s", "n"),
+            ("s", "n"),
+            ("", "n"),
+            ("", "n"),
+            ("x", "m"),
+            ("", ""),
+        ),
+    )
 }
+
